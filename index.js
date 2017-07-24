85006b8fc8e1b9aff6ac2b7a08ab36615be1b6b1
sanlouise <sandra.adamshallie@gmail.com>
2017-07-24 14:45:39 -0400

    Reformat solution

--- index.js
+++ index.js
@@ -1,12 +1,14 @@
-const moment = require('moment');
 const chalk = require('chalk');
-
 const midnight = moment().startOf('day');
+const moment = require('moment');
 const now = moment();
-
 const seconds = moment().valueOf() - moment();
+const year = moment().format("YYYY");
+
 console.log(`It is ${chalk.hex('6CA0B5')(moment().format("dddd, MMMM Do YYYY, h:mm:ss a"))}.`);
 console.log(`It is the ${chalk.hex('A976AF')(moment().format("DDDo"))} day of the year.`);
 console.log(`It is ${chalk.hex('77B4A9')((parseInt((now - midnight) / 1000)))} seconds into the day.`);
 if (moment().isDST()) console.log(`It ${chalk.hex('7BA560')('is')} during Daylight Savings Time.`);
-if (!moment([2017, 1, 29]).isValid()) console.log(`It is ${chalk.hex('AC4242')('not')} a leap year.`)
+if (!moment().isDST()) console.log(`It is ${chalk.hex('AC4242')('not')} during Daylight Savings Time.`);
+if (moment([year]).isLeapYear()) console.log(`It ${chalk.hex('7BA560')('is')} a leap year.`)
+if (!moment([year]).isLeapYear()) console.log(`It is ${chalk.hex('AC4242')('not')} a leap year.`)
